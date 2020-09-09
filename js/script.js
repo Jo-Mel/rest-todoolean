049ef19b31a4ac677c3cc47e46bbe25d9c4cb7a7
Jona M <jonameleqi@gmail.com>
2020-09-09 16:38:25 +0200

    wip

--- js/script.js
+++ js/script.js
@@ -1,24 +1,33 @@
 $(document).ready(function () {
+  getData();
+});
+
+//**** funzioni ****/
+function getData() {
   $.ajax({
     url: "http://157.230.17.132:3020/todos",
     method: "GET",
     success: function (risposta) {
-      var source = $("#entry-template").html();
-      var template = Handlebars.compile(source);
-      for (var i = 0; i < risposta.length; i++) {
-        var context = risposta[i];
-
-        //   var context = {
-        //      text: risposta[i].text,
-        //      id: risposta[i].id
-        //   }
-
-        var html = template(context);
-        $(".todos").append(html);
-      }
+      getItem(risposta);
     },
     error: function () {
       alert("Errore");
     },
   });
-});
+}
+
+function getItem(data) {
+  var source = $("#entry-template").html();
+  var template = Handlebars.compile(source);
+  for (var i = 0; i < data.length; i++) {
+    var context = data[i];
+
+    //   var context = {
+    //      text: data[i].text,
+    //      id: data[i].id
+    //   }
+
+    var html = template(context);
+    $(".todos").append(html);
+  }
+}
